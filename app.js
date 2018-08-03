b8dbeff02eca34a4359c7978a74fd827a8caf710
Almendra <estrada.almendra@gmail.com>
2018-08-03 16:34:35 -0400

    third commit

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const morgan = require('morgan');
 const models = require('./models');
 const wikiRouter = require('./routes/wiki');
 
+
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 app.use('/wiki', wikiRouter);
+
+
+
 
 models.db.authenticate().then(() => {
   console.log('connected to the database');
--- routes/wiki.js
+++ routes/wiki.js
@@ -2,12 +2,14 @@
 const router = require('express').Router();
 const { Page } = require("../models");
 const { addPage } = require("../views");
+const { wikiPage }= require("../views");
+const { User } = require("../models");
 
 router.get('/', (req, res, next) => {
     res.redirect('/wiki');
 });
 
-router.get('/add', (req, res, next) => {
+router.get('/add', async (req, res, next) => {
     res.send(addPage());
 })
 
@@ -19,10 +21,28 @@ router.post('/', async (req, res, next) => {
     try {
         await page.save();
         console.log(page);
-        res.redirect(`/${page.slug}`);
+        res.redirect(`/wiki/${page.slug}`);
     } catch (error) { next(error); }
     //res.json(req.body);
 });
+
+router.get('/:slug', async (req, res, next) => {
+    try{
+    //res.send(`hit dynamic route at ${req.params.slug}`);
+    const foundPage = await Page.findOne({
+        where:{ slug: req.params.slug}
+    })
+    const author = await User.findOne({
+        where:{ name: req.params.name}
+    })
+    res.send(wikiPage(foundPage,author));
+       }
+          catch(error) {next(error)}
+  });
+
+
+
+
 
 module.exports = router;
 
