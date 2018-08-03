f8feb03f9cd7efcac7ac57b5a430860ac67c2e7d
wheyjanc <wchen93@asu.edu>
2018-08-03 12:38:49 -0400

    completed sequelize and db setup

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan');
-
-
-
+const { db } = require('./models');
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
-
-
 app.use(express.urlencoded({ extended: false }));
-
 
 app.get("/", (req, res) => {
   res.send("hello world");
-
-
 });
 
-const port = 3000;
-app.listen(port, () =>{
+db.authenticate().
+  then(() => {
+    console.log('connected to the database');
+  })
 
-  console.log(`App listening in ${port}`);
-})
+const init = async () => {
+  await models.db.sync({force: true});
 
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`App listening in ${port}`);
+  });
+}
 
-
-
+init();
---
+++ models/index.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
+
+module.exports = {
+  db
+};
+
+const Page = db.define('page', {
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'Page title'
+    },
+    slug: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'Page slug'
+    },
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      defaultValue: 'Page content'
+    },
+    status: {
+      type: Sequelize.ENUM('open', 'closed')
+    }
+  });
+  const User = db.define('user', {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'User name'
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      isEmail: true,
+      defaultValue: 'User email'
+    }
+  });
+
+  module.exports = { Page, User };
